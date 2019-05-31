d88b56095a6df15b206d17e45dacf7012bb18b0f
marony <marony@binbo-kodakusan.com>
2019-05-31 10:46:04 +0900

    ステップ2：加減算のできるコンパイラの作成

--- cppm.cpp
+++ cppm.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -6,10 +7,30 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
   std::cout << ".intel_syntax noprefix" << std::endl;
   std::cout << ".global main" << std::endl;
   std::cout << "main:" << std::endl;
-  std::cout << "  mov rax, " << atoi(argv[1]) << std::endl;
+  std::cout << "  mov rax, " << std::strtol(p, &p, 10) << std::endl;
+
+  while (*p) {
+    if (*p == '+') {
+      ++p;
+      std::cout << "  add rax, " << std::strtol(p, &p, 10) << std::endl;
+      continue;
+    }
+
+    if (*p == '-') {
+      ++p;
+      std::cout << "  sub rax, " << std::strtol(p, &p, 10) << std::endl;
+      continue;
+    }
+
+    std::cerr << "予期しない文字です: '" << *p << "'" << std::endl;
+    return 1;
+  }
+
   std::cout << "  ret" << std::endl;
 
   return 0;
